4368c72e317ef65b0cc13d276df674816113d8e6
Andrey Antipov <gorttar@gmail.com>
2016-11-21 14:10:58 +0400

    adding tuple support and defaults

--- src/main/java/control/functions/Fn2.java
+++ src/main/java/control/functions/Fn2.java
@@ -1,11 +1,17 @@
 package control.functions;
 
+import data.tuple.T2;
+
 /**
  * @author Andrey Antipov (gorttar@gmail.com) (2016-11-18 17:18)
  */
 @FunctionalInterface
 public interface Fn2<A, B, Rt> extends Fn1<A, Fn1<B, Rt>> {
     Rt a(A a, B b);
+
+    default Rt a(T2<? extends A, ? extends B> t) {
+        return a(t.a, t.b);
+    }
 
     @Override
     default Fn1<B, Rt> a(A a) {
--- src/main/java/control/functions/Fn3.java
+++ src/main/java/control/functions/Fn3.java
@@ -1,11 +1,26 @@
 package control.functions;
 
+import data.tuple.T2;
+import data.tuple.T3;
+
 /**
  * @author Andrey Antipov (gorttar@gmail.com) (2016-11-18 19:02)
  */
 @FunctionalInterface
 public interface Fn3<A, B, C, Rt> extends Fn1<A, Fn2<B, C, Rt>> {
     Rt a(A a, B b, C c);
+
+    default Rt a(T3<? extends A, ? extends B, ? extends C> t) {
+        return a(t.a, t.b, t.c);
+    }
+
+    default Fn1<C, Rt> a(A a, B b) {
+        return c -> a(a, b, c);
+    }
+
+    default Fn1<C, Rt> a(T2<? extends A, ? extends B> t) {
+        return a(t.a, t.b);
+    }
 
     @Override
     default Fn2<B, C, Rt> a(A a) {
